38b6a06d9e7e8e2a1eb3d7ae2140f8c50eb14f03
Yury Petrov <gderpov@yandex.ru>
2024-02-08 00:44:06 +0500

    Общий класс под клавиатуру
    * Главное меню
    * Функция создания клавиатуры

---
+++ keyboards/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["inline_admin_main_menu", "inline_common_main_menu"]
+
+from .main_keyboard import inline_admin_main_menu, inline_common_main_menu
+
---
+++ keyboards/main_keyboard.py
@@ -0,0 +1,32 @@
+from aiogram.utils.keyboard import InlineKeyboardButton, InlineKeyboardMarkup
+
+from keyboards.menu import MainMenu
+
+
+class Keyboard:
+    def __init__(self):
+        self.main_menu: MainMenu = MainMenu()
+
+    @staticmethod
+    def create_kb(button_layout: list) -> InlineKeyboardMarkup:
+
+        kb = []
+
+        for b_row in button_layout:
+            row = []
+            for button in b_row:
+                text, callback_data = button
+                row.append(InlineKeyboardButton(text=text, callback_data=callback_data))
+            kb.append(row)
+
+        return InlineKeyboardMarkup(inline_keyboard=kb)
+
+
+keyboard = Keyboard()
+
+common_main_menu = keyboard.main_menu.button_layout(admin=False)
+admin_main_menu = keyboard.main_menu.button_layout(admin=True)
+
+inline_common_main_menu = keyboard.create_kb(button_layout=common_main_menu)
+inline_admin_main_menu = keyboard.create_kb(button_layout=admin_main_menu)
+
