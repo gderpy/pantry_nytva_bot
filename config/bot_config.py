7253605622a30692b04f2c344024245a1f48650f
Yury Petrov <gderpov@yandex.ru>
2024-02-08 00:45:25 +0500

    bot_config - список с ID админов

--- config/bot_config.py
+++ config/bot_config.py
@@ -9,11 +9,13 @@ load_dotenv()
 
 class BotConfig(BaseModel):
     API_TOKEN: str = os.getenv("API_TOKEN")
+    ADMINS: list = [228931634]
 
 
 bot_config = BotConfig()
 
 API_TOKEN = bot_config.API_TOKEN
+ADMINS = bot_config.ADMINS
 
 
 
